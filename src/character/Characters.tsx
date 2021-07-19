f966024f9e4c8c7e072f4892f62f4e3b180b1ddd
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-07-19 13:53:17 +0530

    Add config file, minor updates

--- src/character/Characters.tsx
+++ src/character/Characters.tsx
@@ -2,14 +2,15 @@ import { useState, useCallback } from "react";
 import { Link } from "react-router-dom";
 import client from "../client";
 import { GET_CHARACTERS } from "./query";
+import { CharacterPartial } from "../types";
 
 function Characters() {
 
-    const [characters, setCharacters] = useState([]);
+    const [characters, setCharacters] = useState<CharacterPartial[]>([]);
     const [nextPage, setNextPage] = useState(1);
     const [error, setError] = useState("")
 
-    const scrollHandler = useCallback((event: React.UIEvent<HTMLDivElement>) => {
+    const scrollHandler = useCallback((event: React.UIEvent<HTMLDivElement, UIEvent>) => {
         const scrollTop = event.currentTarget.scrollTop;
         const scrollHeight = event.currentTarget.scrollHeight;
         const scrollFraction = scrollTop / scrollHeight;
@@ -22,15 +23,19 @@ function Characters() {
                     page: nextPage
                 }
             })
-            .then({data as CharacterPartial} => )
+            .then(result => {
+                const data = result.data.characters.results as CharacterPartial[];
+                setCharacters([...characters, ...data]);
+            })
+            .catch(error => setError(error.message));
 
             setNextPage(nextPage + 1);
         }
-    })
+    }, [nextPage]);
 
     return (
-        <div onScroll={(event) => {}}>
-
+        <div onScroll={scrollHandler}>
+            
         </div>
     );
 }
